93265a044314c77899e3e4f44e739fa5be18cc95
Yuvraj Singh <me@iyuvraajsingh.com>
2019-08-02 12:06:37 -0700

    Create A Provider Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 
 //Create a new context which will broadcast your data from your parent component to all the child and sub-child components.
 const MyContext = React.createContext();
 
- 
+// Create a provider component
+class MyProvider extends Component {
+  state = {
+    firstName: "John",
+    lastName: "Smith"
+  };
+
+  render() {
+    return (
+      <MyContext.Provider
+        value={{
+          //We are passing the state from the provider to all the child and sub-child components.
+          state: this.state,
+          //This function will update the state of the variables in the provider (in the whole app) whenever it is being called.
+          updateDetails: data => {
+            this.setState({
+              firstName: data.firstName,
+              lastName: data.lastName
+            });
+          }
+        }}
+      >
+        {this.props.children}
+      </MyContext.Provider>
+    );
+  }
+}
+
 function App() {
   return (
     <div className="App">
