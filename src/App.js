4de64e6bd9f06f811baac58dc2b3abc9efe23028
Yuvraj Singh <me@iyuvraajsingh.com>
2019-08-02 12:13:54 -0700

    Taking Input From The User And Update That To Your Provider

--- src/App.js
+++ src/App.js
@@ -50,12 +50,66 @@ class DisplayName extends Component {
   }
 }
 
+// Create a class to take input from the user and update that to your provider and see how DisplayName Component having the current value of the variables in the provider gets updated.
+class UpdateName extends Component {
+  render() {
+    return (
+      // Add a consumer wherever you need to access the data from the provider anywhere in the application.
+      <MyContext.Consumer>
+        {context => (
+          <React.Fragment>
+            <form
+              onSubmit={event => {
+                event.preventDefault();
+                // Get the new value from the user input.
+                const data = {
+                  firstName: event.target.firstName.value,
+                  lastName: event.target.lastName.value
+                };
+                // Call the function which we wrote in the provider and pass in the data coming from the user input. This function will update the state of the variables with the new value coming in.
+                context.updateDetails(data);
+              }}
+            >
+              <label>
+                First Name:
+                <input
+                  type="text"
+                  name="firstName"
+                  style={{ borderWidth: "1px" }}
+                />
+              </label>
+              <br />
+              <br />
+              <label>
+                Last Name:
+                <input
+                  type="text"
+                  name="lastName"
+                  style={{ borderWidth: "1px" }}
+                />
+              </label>
+              <br />
+              <br />
+              <input
+                type="submit"
+                value="Update Names"
+                style={{ borderWidth: "2px" }}
+              />
+            </form>
+          </React.Fragment>
+        )}
+      </MyContext.Consumer>
+    );
+  }
+}
+
 function App() {
   return (
     // Wrap your whole application inside the provider so that you can have access to each and every thing inside provder on the whole applicaiton.
     <MyProvider>
       <div>
         <h1>React Context API Example</h1>
+        <UpdateName />
         <DisplayName />
       </div>
     </MyProvider>
