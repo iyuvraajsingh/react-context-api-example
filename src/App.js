736c5ed88957389e59cd03808358deb92672913c
Yuvraj Singh <me@iyuvraajsingh.com>
2019-08-02 11:08:03 -0700

    Create A New Context

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "./App.css";
 
+//Create a new context which will broadcast your data from your parent component to all the child and sub-child components.
+const MyContext = React.createContext();
+
+ 
 function App() {
   return (
     <div className="App">
